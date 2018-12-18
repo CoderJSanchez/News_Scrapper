2f5beb3b063b132391ccc600056d5072f48e858d
Julio Sanchez <jsanchez191980802@gmail.com>
2018-12-17 23:38:04 -0600

    added dependencies and server is working

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
+const axios = require('axios');
+const  mongoose = require('mongoose');
+const cheerio = require('cheerio');
+
+const port = process.env.port || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.listen(port, function(){
+    console.log("Listening on PORT... " + port);
+});
+
 
